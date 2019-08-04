3a00a048e2d46844aff3ae26fbbb4ad5357d0ac6
JoseAguilar07 <Toledo_JoseAguilar@hotmail.com>
2019-08-04 12:22:40 -0500

    nuevo commit

---
+++ Envoy.blade.php
@@ -0,0 +1,27 @@
+@servers(['aws'=>'ubuntu@3.19.56.195'])
+
+@include ('vendor/autoload.php')
+@setup
+  if (!isset($on)){
+    throw new Exception ('La variable --on no esta definida');
+  }
+  $branch = isset($branch) ? $branch:'master';
+  $origin = 'git@github.com:JoseAguilar07/curso-envoy.aws';
+  $app_dir = '/var/www/html';
+@endsetup
+
+@task('test')
+  echo {{$branch}}
+@endtask
+
+@task('ls'['on'=> $on])
+  cd {{$app_dir}}
+  ls -la
+@endtask
+
+@task('git:clone', ['on' => $on])
+  cd {{$app_dir}}
+  echo "hemos entrado al directorio /var/www/html";
+  git clone {{$origin}}
+  echo "repositorio clonado correctamente";
+@endtask
